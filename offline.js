5f45b5476e492c979369d141f65b240363eb852f
rebeccalenox <7623996+rebeccalenox@users.noreply.github.com>
2023-02-14 20:02:24 -0500

    Add files via upload
    from desktop

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1676307476,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/graphpic-sheet0.png",
+		"images/zero-sheet0.png",
+		"images/half-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
